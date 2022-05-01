ab2e5ee0206f6d0c56437a994bd1458a88af7987
vigneshj04 <vigneshjmax@gmail.com>
2022-05-01 15:40:16 +0530

    Added: Fare Calculation

--- src/CabInvoiceMain.java
+++ src/CabInvoiceMain.java
@@ -1,2 +1,12 @@
+
 public class CabInvoiceMain {
+
+    static InvoiceGenerator invoice = new InvoiceGenerator();
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Cab Services");
+
+        System.out.println(invoice.fareCalculation(1, 4));
+
+    }
 }
--- src/InvoiceGenerator.java
+++ src/InvoiceGenerator.java
@@ -1,2 +1,16 @@
 public class InvoiceGenerator {
+
+    int costPerKm = 10;
+    int costPerMin = 1;
+    int minFare = 5;
+
+    //Calculating total fare....
+    public double fareCalculation(double distance, int time) {
+        double totalFare = distance * costPerKm + time * costPerMin;
+        if (totalFare < minFare) {
+            return minFare;
+        } else {
+            return totalFare;
+        }
+    }
 }
